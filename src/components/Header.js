ddaba89bca9c9124bc0905856d6324662745a177
Mauro Rosales Jr <mauror@jasonsaacstudio.lan>
2023-03-27 11:51:13 -0700

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Header(){
+    return(
+        <React.Fragment>
+            <h1>Tina Marie Jewelry</h1>
+        </React.Fragment>
+    )
+}
+
+export default Header;
