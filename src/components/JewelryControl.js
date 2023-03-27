16379cac5cac2665c82241da3078577f8e6899f2
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2023-03-27 16:44:36 -0700

    rough draft of render done

--- src/components/JewelryControl.js
+++ src/components/JewelryControl.js
@@ -6,15 +6,43 @@ import JewelryList from "./JewelryList;";
 import mainJewelryList from "./MainJewelryList";
 
 class JewelryControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainJewelryList: mainJewelryList,
+      selectedJewelry: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleClick = () => {
+    if(this.state.selectedJewelry != null){
+      this.setState({
+        selectedJewelry: null,
+      })
+    } 
+  }
+
+  handleChangingSelectedJewelry = (id) => {
+    const selectedJewelry = this.state.mainJewelryList.filter(jewelry => jewelry.id === id)[0];
+    this.setState({selectedJewelry: selectedJewelry});
+  }
 
   render(){
-
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.selectedJewelry != null) {
+      currentlyVisibleState = <JewelryDetail jewerly={this.state.selectedJewelry}
+                              />
+    } else {
+      currentlyVisibleState = <JewelryList jewelryList={this.state.mainJewelryList}
+                                            onJewelrySelection={this.handleChangingSelectedJewelry} />;
+    }
 
 
     return (
       <React.Fragment>
-        
+
       </React.Fragment>
     );
   }
