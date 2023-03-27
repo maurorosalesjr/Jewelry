6652fe44e888fbd57987686730e449f49138ab66
Mauro Rosales Jr <mauror@jasonsaacstudio.lan>
2023-03-27 14:51:07 -0700

    finish detail and start with seeded data in main jewelry list dot js

--- src/components/JewelryDetail.js
+++ src/components/JewelryDetail.js
@@ -2,12 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-function JewelryDetail() {
+function JewelryDetail(props) {
+    const { jewerly } = props;
+
+
     return(
         <React.Fragment>
             <div>
-                
+                <img src={jewerly.photo} alt={jewerly.altTag} />
+                <h3>{jewerly.name}</h3>
+                <h6>{jewerly.catagory}</h6>
+                <p>{jewerly.desc}</p>
+                <p><em>{jewerly.price}</em></p>
+                <p>{jewerly.quantity}</p>
             </div>
         </React.Fragment>
     )
 }
+
+JewelryDetail.propTypes = {
+    jewerly: PropTypes.object,
+}
+
+export default JewelryDetail;
---
+++ src/components/MainJewelryList.js
@@ -0,0 +1,48 @@
+import { v4 } from "uuid";
+
+
+let mainJewelryList = [
+    {
+        category: "",
+        name: "",
+        desc: "",
+        price: "",
+        img: "",
+        altTag: "",
+        quantity: "",
+        id: v4()
+    },
+    {
+        category: "",
+        name: "",
+        desc: "",
+        price: "",
+        img: "",
+        altTag: "",
+        quantity: "",
+        id: v4()
+    },
+    {
+        category: "",
+        name: "",
+        desc: "",
+        price: "",
+        img: "",
+        altTag: "",
+        quantity: "",
+        id: v4()
+    },
+    {
+        category: "",
+        name: "",
+        desc: "",
+        price: "",
+        img: "",
+        altTag: "",
+        quantity: "",
+        id: v4()
+    },
+
+]
+
+export default mainJewelryList;
