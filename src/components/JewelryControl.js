d9c2c18f390b72c7f1469df9f94e537d7d094b00
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2023-03-27 16:09:34 -0700

    start of control

---
+++ src/components/JewelryControl.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Jewelry from "./Jewelry";
+import JewelryCategory from "./JewelryCategory";
+import JewelryDetail from "./JewelryDetail";
+import JewelryList from "./JewelryList;";
+import mainJewelryList from "./MainJewelryList";
+
+class JewelryControl extends React.Component {
+
+
+  render(){
+
+
+
+    return (
+      <React.Fragment>
+        
+      </React.Fragment>
+    );
+  }
+
+  
+}
