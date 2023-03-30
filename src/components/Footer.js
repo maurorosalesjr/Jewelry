4b3adf8f70b578aecf93161857b2222189c59f4d
Mauro Rosales Jr <mauror@jasonsaacstudio.lan>
2023-03-30 14:55:13 -0700

    changed the link color in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ButtonCss.css";
 
 const topFoot = {
     backgroundColor: "#a23cf0",
@@ -19,10 +20,10 @@ function Footer(){
             <div style={topFoot}>
                 <h1>Thank you for visting Tina Marie Jewelry</h1>
                 <div style={footerStyle}>
-                <h5>You can find me at:</h5>
-                    <a href="https://www.instagram.com/tina_maries_jewelry/?igshid=YmMyMTA2M2Y%3D" ><p>Instagram</p></a>
-                    <a href="https://www.etsy.com/shop/TinaMariesJewelry" ><p>Etsy</p></a>
-                    <a href="https://www.ebay.com/usr/tina_maries_jewelry"><p>Ebay</p></a>
+                <h5>You can find me at: 
+                    ||<a href="https://www.instagram.com/tina_maries_jewelry/?igshid=YmMyMTA2M2Y%3D" class="link"> Instagram </a>
+                    ||<a href="https://www.etsy.com/shop/TinaMariesJewelry" class="link"> Etsy </a>
+                    ||<a href="https://www.ebay.com/usr/tina_maries_jewelry" class="link"> Ebay </a></h5>
                 </div>
                 <div >
                     <h5>Contact Info: </h5>
