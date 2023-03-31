5a65897816dbca3248bb4a7f7fa2d12b14ea48bd
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2023-03-30 17:02:25 -0700

    fixed call up bug with details, its still WIP

--- src/components/JewelryDetail.js
+++ src/components/JewelryDetail.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import mainJewelryList from './MainJewelryList';
 
 
-function JewelryDetail(mainJewelryList) {
-    const {jewelry} = mainJewelryList;
+
+function JewelryDetail(props) {
 
     return (
-        <div className="details">
-        <h2>{mainJewelryList.jewelry.name}</h2>
-        <img src={jewelry.photo} alt={jewelry.altTag} />
-        <p>{`Price: $${jewelry.price}`}</p>
-        <p>{`Description: ${jewelry.description}`}</p>
+        <div>
+        <h2>{props.item.name}</h2>
+        <img src={props.item.photo} alt={props.item.altTag} />
+        <p>${props.item.price}- only {props.item.quantity} left!</p>
+        <p>{props.item.desc}</p>
+        <button onClick={props.closeDetails}>Close</button>
         </div>
     );
 }
--- src/components/JewelryList.js
+++ src/components/JewelryList.js
@@ -25,43 +25,35 @@ const lowCount = {
 
 
 function JewelryList(props) {
-    const [showDetail, setShowDetail] = useState(false);
-    // const [lowQuant, setLowQuant] = useState(false);
+    // const [showDetail, setShowDetail] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
 
-// const lowQuant = () => {
-//     const quant = mainJewelryList.quantity;
-//     if(quant < 4){
-//         return <p style={lowCount}>Only {quant} left!</p>
-//     } else {
-//         return <p>{quant} remaining.</p>
-//     }
-// }
+    const showDetails = (item) => {
+        setSelectedItem(item);
+    };
 
     return (
-        <div style={genStyle}>
-        {mainJewelryList.map((jewelryItem) => (
-            
-            <div class={genStyle} key={jewelryItem.id}>
-            <img style={imgStyle} src={jewelryItem.photo} alt={jewelryItem.altTag} />
-            <p >{jewelryItem.name} - ${jewelryItem.price}- 
-            {showDetail ? (
-                <JewelryDetail />
-                // <button onClick={() =>setShowDetail(showDetail)}>Back</button>
-            ) : (
-                <button class="button-64" onClick={() =>setShowDetail(!showDetail)}>Details</button>
-            )}</p> 
-            {/* {lowQuant()} */}
-            
-            
-            
-            
-            
+        <div>
+                    {selectedItem && (
+            <JewelryDetail
+            item={selectedItem}
+            closeDetails={() => setSelectedItem(null)}
+            />
+        )}
+        {mainJewelryList.map((item, index) => (
+            <div class={genStyle} key={index}>
+            <img style={imgStyle} src={item.photo} alt={item.altTag} />
+            <p>{item.name}- ${item.price}</p>
+            <div>
+                <button class="button-64" onClick={() => showDetails(item)}>Details</button>
+            </div>
             <br />
-            <button class="button-64" onClick={() => props.addToCart(jewelryItem)}>Add to Cart</button>
+            <button class="button-64" onClick={() => props.addToCart(item)}>Add to Cart</button>
             <hr />
             </div>
         ))}
         </div>
+
     );
     }
 
